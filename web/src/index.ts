2e7cadc6ebdf6f1a88a6eac4870da57c0d8d6dec
alexezh <alexezh@EzhM.local>
2023-11-25 15:52:34 -0800

    switch to react

--- web/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { setFetchAdapter } from "./lib/fetchadapter";
-import { FetchAdapterWeb } from "./lib/fetchadapterweb";
-import { App } from "./app";
-
-// root object for index code
-export var gameApp = new App();
-setFetchAdapter(new FetchAdapterWeb());
-
